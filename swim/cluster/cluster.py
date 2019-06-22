109375fddf8ef01f6cce1e00c2415bb76362ab25
Turner Mandeville <Turner@Turner.fios-router.home>
2019-06-22 07:32:29 -0400

    remove duplicate files

--- swim/cluster/cluster.py
+++
@@ -1,7 +0,0 @@
-from src.node.address import Address
-
-class ClusterConfiguration():
-    def __init__(self):
-        self.joinAddr = Address('localhost', 8888)
-        self.workAddr = Address('localhost', 8889)
-        self.clientAddr = Address('localhost', 8890)
